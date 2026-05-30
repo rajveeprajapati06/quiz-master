66215579cf9cfb4239204da185a0ef94fa8026cf
Jeeldadhaniya <jeeldadhaniya0114@gmail.com>
2026-05-31 02:09:58 +0530

    updated project

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,4 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running in development mode on port ${PORT}`);
-});
+app.listen(PORT, () => console.log("Server running"));
